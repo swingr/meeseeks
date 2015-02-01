674622c3b70e96c4322dab021c7b6521f7b72178
Derek-lai- <derek_lai_@hotmail.com>
2015-01-31 19:34:03 -0500

    added .wav files, removed mp3, changed audio

--- audio.py
+++ audio.py
@@ -2,25 +2,25 @@ from pydub import AudioSegment
 from pydub.playback import play
 
 def start():
-    song = AudioSegment.from_mp3("audio/beginning.mp3")
+    song = AudioSegment.from_wav("audio/beginning.wav")
     play(song)
 
 def shoulders():
-    song = AudioSegment.from_mp3("audio/shoulders.mp3")
+    song = AudioSegment.from_wav("audio/shoulders.wav")
     play(song)
 
 def choke():
-    song = AudioSegment.from_mp3("audio/choke.mp3")
+    song = AudioSegment.from_wav("audio/choke.wav")
     play(song)
 
 def existence():
-    song = AudioSegment.from_mp3("audio/existance.mp3")
+    song = AudioSegment.from_wav("audio/existance.wav")
     play(song)
 
 def frustrating():
-    song = AudioSegment.from_mp3("audio/frustrating.mp3")
+    song = AudioSegment.from_wav("audio/frustrating.wav")
     play(song)
 
 def head():
-    song = AudioSegment.from_mp3("audio/head.mp3")
+    song = AudioSegment.from_wav("audio/head.wav")
     play(song)
