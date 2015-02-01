281429cca9edd1f739d8d28a0d585be9c8a641b3
David Kua <david@kua.io>
2015-01-31 20:51:04 -0500

    Fix import errors

--- __init__.py
+++ __init__.py
@@ -2,5 +2,6 @@ __title__ = 'meeseeks'
 __version__ = '0.0.1'
 __build__ = 0x01
 __license__ = 'MIT'
+__all__ = ["audio", "pebble"]
 
 from .meeseeks import *
--- audio/audio.py
+++ audio/audio.py
@@ -1,26 +1,32 @@
 from pydub import AudioSegment
 from pydub.playback import play
+from os import path
+
+_AUDIO_DIR = path.join(path.dirname(__file__), '')
+
+def _format(sound_file):
+    return "{0}{1}".format(_AUDIO_DIR, sound_file)
 
 def start():
-    song = AudioSegment.from_wav("audio/beginning.wav")
+    song = AudioSegment.from_wav(_format("beginning.wav"))
     play(song)
 
 def shoulders():
-    song = AudioSegment.from_wav("audio/shoulders.wav")
+    song = AudioSegment.from_wav(_format("shoulders.wav"))
     play(song)
 
 def choke():
-    song = AudioSegment.from_wav("audio/choke.wav")
+    song = AudioSegment.from_wav(_format("choke.wav"))
     play(song)
 
 def existence():
-    song = AudioSegment.from_wav("audio/existance.wav")
+    song = AudioSegment.from_wav(_format("existence.wav"))
     play(song)
 
 def frustrating():
-    song = AudioSegment.from_wav("audio/frustrating.wav")
+    song = AudioSegment.from_wav(_format("frustrating.wav"))
     play(song)
 
 def head():
-    song = AudioSegment.from_wav("audio/head.wav")
+    song = AudioSegment.from_wav(_format("head.wav"))
     play(song)
